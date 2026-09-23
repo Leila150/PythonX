b2d509bd363515fbc76ab9e0c8ef4184e56c39b9
Leila150 <esguerradominguita@gmail.com>
2026-09-23 17:36:46 +0800

    Implement PythonX root object type

---
+++ Objects/pythonxobject.c
@@ -0,0 +1,70 @@
+#include "Python.h"
+#include "pycore_pythonxobject.h"
+
+#include <structmember.h>
+
+static int
+pythonx_object_traverse(PyXObject *self, visitproc visit, void *arg)
+{
+    Py_VISIT(self->dict);
+    return 0;
+}
+
+static int
+pythonx_object_clear(PyXObject *self)
+{
+    Py_CLEAR(self->dict);
+    return 0;
+}
+
+static void
+pythonx_object_dealloc(PyXObject *self)
+{
+    PyTypeObject *type = Py_TYPE(self);
+    PyObject_GC_UnTrack(self);
+    pythonx_object_clear(self);
+    type->tp_free((PyObject *)self);
+    Py_DECREF(type);
+}
+
+static PyObject *
+pythonx_object_repr(PyObject *self)
+{
+    PyTypeObject *type = Py_TYPE(self);
+    return PyUnicode_FromFormat("<PythonX %s object at %p>",
+                               type->tp_name, self);
+}
+
+static PyMemberDef pythonx_object_members[] = {
+    {"__dict__", T_OBJECT, offsetof(PyXObject, dict), READONLY,
+     "PythonX instance attribute dictionary."},
+    {NULL}
+};
+
+PyTypeObject PyXObject_Type = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "pythonx.object",
+    .tp_basicsize = sizeof(PyXObject),
+    .tp_dealloc = (destructor)pythonx_object_dealloc,
+    .tp_repr = pythonx_object_repr,
+    .tp_as_async = NULL,
+    .tp_flags = Py_TPFLAGS_DEFAULT |
+                Py_TPFLAGS_BASETYPE |
+                Py_TPFLAGS_HAVE_GC,
+    .tp_doc = "PythonX root object type.",
+    .tp_traverse = (traverseproc)pythonx_object_traverse,
+    .tp_clear = (inquiry)pythonx_object_clear,
+    .tp_members = pythonx_object_members,
+    .tp_getattro = PyObject_GenericGetAttr,
+    .tp_setattro = PyObject_GenericSetAttr,
+    .tp_dictoffset = offsetof(PyXObject, dict),
+    .tp_alloc = PyType_GenericAlloc,
+    .tp_free = PyObject_GC_Del,
+    .tp_new = PyType_GenericNew,
+};
+
+PyObject *
+_PyX_NewObject(PyTypeObject *type)
+{
+    return type->tp_new(type, NULL, NULL);
+}
